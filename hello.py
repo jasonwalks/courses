12d16218027e5016a367518c08357061e9150df9
jasonwalks <43026914+jasonwalks@users.noreply.github.com>
2024-09-23 11:56:31 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+'''
 # Ask the user for their name
 name = input("what's your name? ").strip().title()
 
@@ -6,3 +7,15 @@ first, last = name.split(" ")
 
 # Greet the user
 print(f"Hello, {first}")
+'''
+
+def main():
+  name = input("What's your name? ")
+  print(hello(name))
+
+def hello(to="world"):
+  print(f"Hello, {to}")
+
+
+if __name__ == "__maim__":
+  main()
