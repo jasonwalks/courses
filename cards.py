8381b8913a8df76539eada98999ec0a4f8dd7d39
jasonwalks <43026914+jasonwalks@users.noreply.github.com>
2024-09-23 12:17:07 +0800

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,12 @@
+import random
+
+cards = ["jack", "queen", "king"]
+
+def main():
+#    random.seed(0)
+#  print(random.choices(cards, k=2))
+   print(random.sample(cards, k=2))
+    
+
+
+main()
---
+++ students.py
@@ -0,0 +1,8 @@
+students = {
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Hermione": "Gryffindor",
+    "Draco": "Slytherin",
+}
+for student in students:
+    print(student)
