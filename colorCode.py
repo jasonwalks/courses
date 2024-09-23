179c8e7d9b0a4751b2c414da50fa27a028d67955
jasonwalks <43026914+jasonwalks@users.noreply.github.com>
2024-09-23 23:27:53 +0800

    Update colorCode.py

--- colorCode.py
+++ colorCode.py
@@ -2,7 +2,7 @@ import re
 
 def main():
   code = input("Hexadecimal color code: ")
-  pattern = r"^#[0-9a-fA-F]{6}"
+  pattern = r"^#[0-9a-fA-F]{6}$"
   
   if match := re.search(pattern, code):
     print(f"Valid. Matched with {match.group()}")
